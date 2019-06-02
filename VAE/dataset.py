3270b42d7cb1cf6bf54126ebfb2b10e1cf5a71ba
HyoungHo Jung <hhjung1202@naver.com>
2019-06-03 08:10:44 +0900

    aaa

--- VAE/dataset.py
+++ VAE/dataset.py
@@ -168,7 +168,7 @@ class USPS(data.Dataset):
         """
         img, label = self.train_data[index, ::], self.train_labels[index]
         print(img.shape)
-        img = Image.fromarray(img, mode='L')
+        img = Image.fromarray(np.squeeze(img), mode='L')
         if self.transform is not None:
             img = self.transform(img)
         label = torch.LongTensor([np.int64(label).item()])
