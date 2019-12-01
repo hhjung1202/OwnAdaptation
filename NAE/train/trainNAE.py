9d8b5d1fe1d2a5228389b614314147cbe041e1c2
HyoungHo Jung <hhjung1202@naver.com>
2019-12-02 05:59:28 +0900

    Time Check

--- NAE/train/trainNAE.py
+++ NAE/train/trainNAE.py
@@ -32,9 +32,9 @@ class Memory(object):
     def Calc_Vector(self, eps=1e-9): # After 1 Epoch, it will calculated
         starttime = time.time()
 
-        mean_len = self.vector.clone().mean(dim=0).pow(2).sum().sqrt() + eps
+        mean_len = self.vector.mean(dim=0).pow(2).sum().sqrt() + eps
         len_mean = self.vector.pow(2).sum(dim=1).sqrt().mean()
-        self.mean_v = self.vector.mean(dim=0) * len_mean / mean_len
+        self.mean_v = self.vector.mean(dim=0) * len_mean.clone() / mean_len
         self.sigma_v = self.vector.var(dim=0).sqrt()
         self.len_v = len_mean
 
@@ -52,7 +52,7 @@ class Memory(object):
 
         if self.index >= self.N:
             self.index = 0
-        self.z[self.index] = z.clone()
+        self.z[self.index] = z
         self.index = self.index + 1
 
         print_time(starttime, 'Memory : Insert_memory')
@@ -62,7 +62,7 @@ class Memory(object):
 
         if self.index2 >= self.N:
             self.index2 = 0
-        self.vector[self.index2] = vector.clone()
+        self.vector[self.index2] = vector
         self.index2 = self.index2 + 1
 
         print_time(starttime, 'Memory : Insert_vector')
