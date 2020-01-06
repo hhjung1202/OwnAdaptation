d12ce93f504e93c386e13000bdcfa0ab2a2017d4
HyoungHo Jung <hhjung1202@naver.com>
2020-01-06 22:47:50 +0900

    db

--- NAE/dataset.py
+++ NAE/dataset.py
@@ -199,7 +199,8 @@ def temp_loader(args):
 
     # Baseline result
     Train_dataset = datasets.CIFAR10(root=root, train=True, transform=transform_train, download=True)
-    print(Train_dataset.shape)
+    print(len(Train_dataset))
+    print(Train_dataset.size)
 
     Temp_loader = torch.utils.data.DataLoader(dataset=Train_dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.workers)
     return Temp_loader
