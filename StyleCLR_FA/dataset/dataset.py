a421e2cfaa8b1b043810555075de80d3f9e02a2d
hhjung1202 <hhjung1202@naver.com>
2020-08-20 16:56:04 +0900

    error

--- StyleCLR_FA/dataset/dataset.py
+++ StyleCLR_FA/dataset/dataset.py
@@ -17,14 +17,12 @@ def Semi_Cifar10_dataset(args):
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize(mean=(0.5, 0.5, 0.5),
-                             std=(0.5, 0.5, 0.5))
+        
     ])
 
     transform_test = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize(mean=(0.5, 0.5, 0.5),
-                             std=(0.5, 0.5, 0.5))
+        
     ])
     
 
