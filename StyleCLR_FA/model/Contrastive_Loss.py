81f2dfba851a5c4b6945e7edb0c394d5525add55
HyoungHo Jung <hhjung1202@naver.com>
2020-10-16 21:12:29 +0900

    last research on

--- StyleCLR_FA/model/Contrastive_Loss.py
+++ StyleCLR_FA/model/Contrastive_Loss.py
@@ -42,15 +42,15 @@ class Style_Contrastive(nn.Module):
         self.softmin = nn.Softmin(dim=-1)
         self.LongTensor = torch.cuda.LongTensor if torch.cuda.is_available() else torch.LongTensor
 
-    def style_contrastive(self, content, style, b, n):
-        f_c = F.normalize(self.gram_matrix(content), p=1, dim=-1).view(b,1,-1)             # b, n, ch * ch
-        f_s = F.normalize(self.gram_matrix(style), p=1, dim=-1).view(1,b,-1)  # b, n, ch * ch
+    def style_contrastive(self, content, style, b):
+        f_c = F.normalize(self.gram_matrix(content), p=1, dim=-1).view(b,1,-1)
+        f_s = F.normalize(self.gram_matrix(style), p=1, dim=-1).view(1,b,-1)
 
         mse = ((f_c - f_s)**2).sum(dim=2).view(b,b)
         return mse
 
-    def forward(self, content, style, b, n):
-        mse = self.style_contrastive(content, style, b, n)
+    def forward(self, content, style, b):
+        mse = self.style_contrastive(content, style, b)
         return mse
     
     def gram_matrix(self, input):
